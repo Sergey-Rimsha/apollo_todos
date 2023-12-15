565bac61db0bb634a630613ffc96d41284c66cf8
Сергей Римша <Sergey.Rimsha.ru@gmail.com>
2023-12-15 14:20:29 +0300

    #3 add useMutation, create todo

--- src/apollo/todos.js
+++ src/apollo/todos.js
@@ -9,3 +9,23 @@ export const ALL_TODO = gql`
 		}
 	}
 `;
+
+export const ADD_TODO = gql`
+	mutation AddTodo($title: String!, $userId: ID!, $completed: Boolean!) {
+		newTodo: createTodo(title: $title, user_id: $userId, completed: $completed) {
+			id
+			title
+			completed
+		}
+	}
+`;
+
+// export const ADD_TODO = gql`
+//   mutation AddTodo($title:String!, $userId: ID!, $completed:Boolean!){
+//     newTodo:createTodo(title:$title, user_id: $userId, completed: $completed) {
+//       id
+//       title
+//       completed
+//     }
+//   }
+// `;
--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,33 +1,43 @@
-import { useState } from 'react';
-import {
-  Button,
-  FormControl,
-  Input,
-} from '@chakra-ui/react';
+import {useState} from 'react';
+import {Button, FormControl, Input,} from '@chakra-ui/react';
+import {useMutation} from "@apollo/client";
+import {ADD_TODO} from "../apollo/todos";
 
 const AddTodo = () => {
-  const [text, setText] = useState('');
+	const [text, setText] = useState('');
+	const [addTodo, {error}] = useMutation(ADD_TODO);
 
-  const handleAddTodo = () => {
-    if (text.trim().length) {
-      setText('');
-    }
-  }
+	const handleAddTodo = () => {
+		if (text.trim().length) {
+			addTodo({
+				variables: {
+					title: text,
+					completed: false,
+					userId: 123,
+				},
+			});
+			setText('');
+		}
+	}
 
-  const handleKey = (event) => {
-    if (event.key === "Enter") handleAddTodo();
-  }
+	const handleKey = (event) => {
+		if (event.key === "Enter") handleAddTodo();
+	}
 
-  return (
-    <FormControl display={'flex'} mt={6}>
-      <Input
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        onKeyPress={handleKey}
-      />
-      <Button onClick={handleAddTodo}>Add todo</Button>
-    </FormControl>
-  );
+	if (error) {
+		return <div>Error...</div>
+	}
+
+	return (
+		<FormControl display={'flex'} mt={6}>
+			<Input
+				value={text}
+				onChange={(e) => setText(e.target.value)}
+				onKeyPress={handleKey}
+			/>
+			<Button onClick={handleAddTodo}>Add todo</Button>
+		</FormControl>
+	);
 };
 
 export default AddTodo;
